e684668cb1dd819a5e8cc8c68e4901a91dbe23b8
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-02-20 17:07:24 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,13 @@
+public class TypeCasting {
+  public static void main(String[] args) {
+    int a = 9;
+    double b = a; 
+    System.out.println(a);      
+    System.out.println(b); 
+
+    double c = 16.18;
+    int d = (int) c; 
+    System.out.println(c);   
+    System.out.println(d); 
+  }
+}
