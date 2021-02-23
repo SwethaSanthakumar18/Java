92557ef86da9d327a89d105afa969190aa9b3f3e
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-02-23 11:42:23 +0530

    Update Developer.java

--- Inheritance/Hierarchical Inheritance/Developer.java
+++ Inheritance/Hierarchical Inheritance/Developer.java
@@ -1,32 +1,32 @@
 class Developer extends Employee
 {
- int no_of_developers;
- long avg_salary;
- int working_hours;
-public int getNo_of_developers() {
- return no_of_developers;
+ int noOfDevelopers;
+ long avgSalary;
+ int workingHours;
+public int getNoOfDevelopers() {
+ return noOfDevelopers;
 }
 
-public void setNo_of_developers(int no_of_developers) {
- this.no_of_developers = no_of_developers;
+public void setNoOfDevelopers(int noOfDevelopers) {
+ this.noOfDevelopers = noOfDevelopers;
 }
 public long getSalary() {
- return avg_salary;
+ return avgSalary;
 }
-public void setSalary(long avg_salary) {
- this.avg_salary = avg_salary;
+public void setSalary(long avgSalary) {
+ this.avgSalary = avgSalary;
 }
-public int getWorking_hours() {
- return working_hours;
+public int getWorkingHours() {
+ return workingHours;
 }
-public void setWorking_hours(int working_hours) {
- this.working_hours = working_hours;
+public void setWorkingHours(int workingHours) {
+ this.workingHours = workingHours;
 }
-void display_developerdetails()
+void displayDeveloperDetails()
    {
-    System.out.println("No of Developers:" +no_of_developers);
-    System.out.println("Average Pay:" +avg_salary);
-    System.out.println("Working Hours:" +working_hours);
+    System.out.println("No of Developers:" +noOfDevelopers);
+    System.out.println("Average Pay:" +avgSalary);
+    System.out.println("Working Hours:" +workingHours);
     }
 
 }
