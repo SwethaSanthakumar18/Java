486fdbf3841876a8d26d130adc5839653c448246
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-02-23 11:36:28 +0530

    Update Method_Overloading.java

--- Method_Overloading.java
+++ Method_Overloading.java
@@ -1,4 +1,4 @@
-class Method_Overloading{  
+class MethodOverloading{  
      public int sum(int arr[]) 
      { 
          int sum = 0; 
@@ -19,7 +19,7 @@ public static void main(String[] args){
 
 int rating[] = {5,4,4,4,2,3,3,5,2,1,3};
 float rating1[] ={4.5f,3.2f,4.4f,2.5f,2.1f,3.5f,4.8f};
-Method_Overloading a=new Method_Overloading();
+MethodOverloading a=new MethodOverloading();
 System.out.println("Sum of Rating:" +a.sum(rating)); 
 System.out.println("Sum of Rating:" +a.sum(rating1));   
 }}  
