25ec1a556e4ad7de39f57020ea95caba8a59a169
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-02-23 11:48:29 +0530

    Update Intern.java

--- Method Overriding/Intern.java
+++ Method Overriding/Intern.java
@@ -1,8 +1,11 @@
 class Intern extends Tasks{   
   void run(){System.out.println("It's the duty of the intern to complete all the tasks before deadline");}  
-  
   public static void main(String args[]){  
   Intern obj = new Intern();  
-  obj.run();
+  obj.run(); //derived class
+  Tasks obj1 = new Tasks();
+  obj1.run(); //base class
+  Tasks obj2 = new Intern();
+  obj2.run(); //derived class
   }  
 }  
