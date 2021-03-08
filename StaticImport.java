4343157e6f7cade041effd957758ae105f98244f
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-03-08 17:35:49 +0530

    Add files via upload

---
+++ StaticImport.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.*; 
+import static java.lang.System.*; 
+class StaticImport { 
+    public static void main(String[] args) 
+    { 
+        out.println(sqrt(4)); 
+        out.println(pow(2, 2)); 
+        out.println(abs(6.3)); 
+    } 
+} 
