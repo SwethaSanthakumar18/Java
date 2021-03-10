3e4462c6be8cab220d76b621db7814adbbf74a69
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-03-10 17:25:38 +0530

    Add files via upload

---
+++ SmallestAndLargest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SmallestAndLargest {
+  public static void main(String[] args) 
+    {
+       
+        Integer arr[] = new Integer[]{12, 3, 5, 7, 19}; 
+        int k = 2; 
+        
+		System.out.println("Original Array: ");
+		System.out.println(Arrays.toString(arr));
+		System.out.println("K'th smallest element of the said array: ");
+        Arrays.sort(arr);       		
+        System.out.print(arr[k-1] + " ");
+		System.out.println("\nK'th largest element of the said array:");          
+        Arrays.sort(arr, Collections.reverseOrder());  		
+        System.out.print(arr[k-1] + " ");
+	}
+}
