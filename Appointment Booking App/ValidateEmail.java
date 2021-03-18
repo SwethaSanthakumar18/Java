0f17e0b1c9e7e4b90bb824b1e6b1dbf283b74572
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-03-18 12:37:03 +0530

    Add files via upload

---
+++ Appointment Booking App/ValidateEmail.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher; 
+import java.util.regex.Pattern; 
+import java.util.Scanner;
+  
+class ValidateEmail
+{ 
+    public static boolean isValid(String email) 
+    { 
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                            "[a-zA-Z0-9_+&*-]+)*@" + 
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                            "A-Z]{2,7}$"; 
+                              
+        Pattern pat = Pattern.compile(emailRegex); 
+        if (email == null) 
+            return false; 
+        return pat.matcher(email).matches(); 
+    } 
+     
+} 
