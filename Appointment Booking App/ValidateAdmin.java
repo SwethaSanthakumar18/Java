b23a84cfc7e8ddb5402bc45da96e15fb8a6112f9
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-03-18 15:54:56 +0530

    Update ValidateAdmin.java

--- Appointment Booking App/ValidateAdmin.java
+++ Appointment Booking App/ValidateAdmin.java
@@ -3,25 +3,25 @@ import java.io.*;
 import java.util.*; 
 public class ValidateAdmin
 {
-  void validateAdminDetails() throws IOException{
-	  String user="admin";
-	  String pass="admin@123";
-   Scanner sc=new Scanner(System.in);
+void validateAdminDetails() throws IOException{
+String user="admin";
+String pass="admin@123";
+Scanner sc=new Scanner(System.in);
 System.out.println("Enter Username");
 String userName = sc.nextLine();
 System.out.println("Enter Password");
 String password = sc.nextLine();
 if(userName.equals(user) && password.equals(pass))
 {
-	System.out.println("Login Successful");
+System.out.println("Login Successful");
 }
 else{
-	System.out.println("Invalid Username and Password");
-	System.exit(0); 
+System.out.println("Invalid Username and Password");
+System.exit(0); 
 }
 }
 /*public static void main(String args[]) throws IOException{
-	ValidateAdmin v = new ValidateAdmin();
-	v.validateAdminDetails();
+ValidateAdmin v = new ValidateAdmin();
+v.validateAdminDetails();
 }*/
 }
