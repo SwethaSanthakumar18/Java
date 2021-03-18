182412c6db3d98837a141755da4b6bcf4d189659
SwethaSanthakumar18 <79361303+SwethaSanthakumar18@users.noreply.github.com>
2021-03-18 12:20:19 +0530

    Add files via upload

---
+++ Appointment Booking App/ValidateAdmin.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.*; 
+import java.util.*; 
+public class ValidateAdmin
+{
+  void validateAdminDetails() throws IOException{
+	  String user="admin";
+	  String pass="admin@123";
+   Scanner sc=new Scanner(System.in);
+System.out.println("Enter Username");
+String userName = sc.nextLine();
+System.out.println("Enter Password");
+String password = sc.nextLine();
+if(userName.equals(user) && password.equals(pass))
+{
+	System.out.println("Login Successful");
+}
+else{
+	System.out.println("Invalid Username and Password");
+	System.exit(0); 
+}
+}
+/*public static void main(String args[]) throws IOException{
+	ValidateAdmin v = new ValidateAdmin();
+	v.validateAdminDetails();
+}*/
+}
---
+++ Appointment Booking App/ValidateCustomer.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.io.*; 
+import java.util.*; 
+public class ValidateCustomer
+{
+	public String name1;
+	public long num;
+  void validateCustomerDetails() throws IOException{
+   String customerFilePath = "C:/Users/Admin/Desktop/Intern/Java/AB/customerdata.txt";
+   ArrayList<Long> arr = new ArrayList<Long>(); 
+   ArrayList<String> arr1 = new ArrayList<String>(); 
+   String line;
+   int i=1;
+   int count=0;
+     String[] customerName = new String[100];
+	 Long[] mobile = new Long[100];
+   Scanner sc=new Scanner(System.in);
+System.out.println("Enter Username");
+name1 = sc.nextLine();
+System.out.println("Enter Password");
+num = sc.nextLong();
+int x=0;
+   BufferedReader reader = new BufferedReader(new FileReader(customerFilePath));
+    while ((x < customerName.length) && (line = reader.readLine()) != null)
+    {
+        String[] parts = line.split(":", 2);
+        if (parts.length >= 2)
+        {
+			customerName[x]=parts[0];
+			mobile[x]=Long.parseLong(parts[1]); 
+			arr.add(mobile[x]);
+			arr1.add(customerName[x]);
+			count++;
+        } else {
+            System.out.println("ignoring line: " + line);
+        }
+    }
+	if(arr.contains(num) && arr1.contains(name1))
+	{
+		System.out.println("Login Successful");
+	}else{
+		System.out.println("Invalid Username and Password");
+		System.exit(0); 
+	}
+}
+/*public static void main(String args[]) throws IOException{
+	ValidateCustomer v = new ValidateCustomer();
+	v.validateCustomerDetails();
+}*/
+}
